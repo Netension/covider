97c9974aa71c45ddbe22718274280c2fa9ffd2ed
Mihucza Bence <mihben@gmail.com>
2021-06-12 18:39:27 +0200

    Add CI pipeline
    #1

---
+++ src/Netension.Covider.Test.Unit/Pipeline_test.cs
@@ -0,0 +1,31 @@
+﻿using Netension.Covider.Application;
+using Xunit;
+
+namespace Netension.Covider.Test.Unit
+{
+    public class Pipeline_test
+    {
+        [Fact(DisplayName = "[UNT-PLT0001][Success]: Pipeline test")]
+        [Trait("Feature", "Pipeline test")]
+        public void PipelineTest_Success()
+        {
+            Assert.True(true);
+        }
+
+        [Fact(DisplayName = "[UNT-PLT0002][Failure]: Pipeline test")]
+        [Trait("Feature", "Pipeline test")]
+        public void PipelineTest_Failure()
+        {
+            Assert.True(false);
+        }
+
+        [Fact(DisplayName = "[UNT-PLT0003][Success]: Coverage")]
+        [Trait("Feature", "Pipeline test")]
+        public void PipelineTest_Coverage()
+        {
+            new Service().Test();
+
+            Assert.True(true);
+        }
+    }
+}
